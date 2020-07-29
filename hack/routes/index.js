1e573f6012c897b815dd7b1edfd134fe43f55bae
DokdoGalmaegi <sdh190323@sdh.hs.kr>
2020-07-30 07:17:00 +0900

    소량의 주석 추가

--- hack/routes/index.js
+++ hack/routes/index.js
@@ -10,7 +10,7 @@ const Content = require('../models/content');
 const { emitWarning } = require('process');
 const { fstat, readFile, renameSync } = require('fs');
 
-/* GET home page. */
+// GET main page.
 router.get('/', function(req, res, next) {
   Content.find((err, content) => {
     if (err) return console.error(err);
@@ -19,6 +19,7 @@ router.get('/', function(req, res, next) {
   });
 });
 
+// GET login page.
 router.get('/login', (req, res, next) => {
   if (req.session) { // session의 값이 null or undefined 확인
     res.render('login');
@@ -27,6 +28,7 @@ router.get('/login', (req, res, next) => {
   }
 });
 
+// GET register page.
 router.get('/register', (req, res, next) => {
   if (req.session) {
     res.render('register', {fail : null});
@@ -35,11 +37,13 @@ router.get('/register', (req, res, next) => {
   }
 })
 
+// Content Download Response
 router.get('/download/:file', (req, res, next) => {
   console.log(req.params.file);
   res.download(`./uploads/${req.params.file}`);
 })
 
+// Register Response
 router.post('/register', (req, res, next) => {
   let newUser = new User();
   User.exists({ id: req.body.id }, (err, isexists) => {
@@ -60,22 +64,25 @@ router.post('/register', (req, res, next) => {
   })
 })
 
+// Login Response
 router.post('/login', (req, res, next) => {
   console.log(req.body);
   User.find({ $and: [{ id: req.body.id }, { pw: req.body.pw }] }, (err, user) => {
-    if (err) return console.error(err);
+    if (err) return res.render('register', { fail: "존재하지 않는 아이디입니다." });
     if (user.length < 1) return res.error("찾아오는 계정이 초과했습니다.");
     req.session.name = user[0].name;
     return res.redirect('/');
   })
 })
 
+// Logout Response
 router.post('/logout', (req, res, next) => {
   if (!req.session) return res.redirect('/');
   req.session.destroy();
   res.redirect('/');
 })
 
+// Write Content Response
 router.post('/writebox', (req, res, next) => {
   console.log(req.files);
   let newContent = new Content();
@@ -101,34 +108,28 @@ router.get('/home', (req, res, next) => {
 
 
 
-
-
-
-
-
-
-router.get('/result', (req, res, next) => {
+router.post('/userinfo', (req, res, next) => { // 유저 정보 받아오기
   User.find((err, user) => {
     if (err) return console.error(err);
     return res.json(user);
   });
 })
 
-router.get('/contentlog', (req, res, next) => {
+router.post('/contentlog', (req, res, next) => { // 게시판 정보 받아오기
   Content.find((err, content) => {
     if (err) return console.error(err);
     return res.json(content);
   });
 })
 
-router.get('/result/delete', (req, res, next) => {
+router.post('/userinfo/delete', (req, res, next) => { // user정보 관리 (삭제)
   User.remove({ $and: [{ id: req.body.id }, { pw: req.body.pw }, { name: req.body.name }] }, (err, user) => {
     if (err) return console.error(err);
     return res.json(user);
   });
 })
 
-router.get('/contentlog/delete', (req, res, next) => {
+router.post('/contentlog/delete', (req, res, next) => { // 게시판 정보 관리 (삭제)
   Content.remove({ title: req.body.title }, (err, content) => {
     if (err) return console.error(err);
     return res.json(content);
